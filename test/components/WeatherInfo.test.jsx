fcdf1a6d3669f6dc16c0b4bd06b86d0606d2f659
Rediat <yaidercc@gmail.com>
2022-09-26 17:03:15 -0500

    implementing tests with jest

---
+++ test/components/WeatherInfo.test.jsx
@@ -0,0 +1,7 @@
+describe('Pruebas en <AddCategory/>', () => { 
+    test('Debe cambiar el valor de la caja de texto ', () => { 
+        const a=1;
+        const b=2;
+        expect(a+b).toBe(6)
+    });
+})
